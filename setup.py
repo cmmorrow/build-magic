2167a5ef3c82b03f0f9b698e3ea185e3434c6ea2
Chris Morrow <cmmorrow@gmail.com>
2020-11-24 18:37:05 -0600

    Added basic setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='build-magic',
+    version='0.1.0',
+    packages=['tests', 'build_magic'],
+    url='',
+    license='',
+    author='Chris Morrow',
+    author_email='cmmorrow@gmail.com',
+    description='A general purpose build/install/deploy tool.'
+)
